feca387633a259f4d51c148ff02a15d35e8da04b
wndud <ju03209@naver.com>
2025-06-13 13:06:55 +0900

    feat: 디테일 페이지 포켓몬 상세정보 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectorPokemonById } from "../RTK/selector";
 
 export default function Detail() {
+  const { pokemonId } = useParams();
+  console.log(typeof pokemonId);
+  const pokemon = useSelector(selectorPokemonById(Number(pokemonId)));
+  console.log(pokemon);
+
+  if (!pokemon) {
+    return <div>포켓몬을 불러오는 중...</div>;
+  }
+
   return (
     <>
-      <p>Detail page</p>
+      <div className="flex flex-col justify-center items-center bg-slate-50 p-[30px] rounded-[10px]">
+        <div className="text-[28px] mb-[10px]">{pokemon.name}</div>
+        <div className="whitespace-pre-wrap text-center mt-4 mb-2 leading-6">
+          {pokemon.description}
+        </div>
+        <img className="w-[200px]" src={pokemon.front} />
+      </div>
     </>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,11 +3,11 @@ import { useSelector } from "react-redux";
 const Card = lazy(() => import("../components/Card.jsx"));
 
 export default function Main() {
-  const pokemonData = useSelector((state) => state.pokemon);
+  const pokemonData = useSelector((state) => state.pokemon.data);
   return (
     <>
       <Suspense fallback={<div> 로딩중...</div>}>
-        {pokemonData.data.map((el) => (
+        {pokemonData.map((el) => (
           <Card key={el.id} pokemon={el} />
         ))}
       </Suspense>
